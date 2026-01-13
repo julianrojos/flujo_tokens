8219c0bc68eea811023672ccac9352725607c9bf
Julián Rojo Soria <julianrojos@gmail.com>
2026-01-13 17:19:08 +0100

    Update Design Tokens

---
+++ Typographyprimitives.ts
@@ -0,0 +1,43 @@
+/**
+ * _typographyPrimitives tokens used in the theme.
+ *
+ */
+
+export const Typographyprimitives = {
+    family: {
+        inter: 'Inter'
+    },
+    weight: {
+        bold: 'Bold',
+        semiBold: 'Semi Bold',
+        medium: 'Medium',
+        regular: 'Regular'
+    },
+    size: {
+        size11: '11px',
+        size12: '12px',
+        size13: '13px',
+        size14: '14px',
+        size16: '16px',
+        size18: '18px',
+        size20: '20px',
+        size24: '24px',
+        size28: '28px',
+        size36: '36px',
+        size48: '48px',
+        size60: '60px',
+        size72: '72px'
+    },
+    lineheight: {
+        lineheight16: '16px',
+        lineheight20: '20px',
+        lineheight24: '24px',
+        lineheight28: '28px',
+        lineheight32: '32px',
+        lineheight36: '36px',
+        lineheight44: '44px',
+        lineheight60: '60px',
+        lineheight72: '72px',
+        lineheight80: '80px'
+    }
+};
---
+++ colors.ts
@@ -0,0 +1,69 @@
+/**
+ * Colors tokens used in the theme.
+ *
+ */
+
+export const Colors = {
+    backgroundcolors: {
+        background01: '{Colorprimitives.neutral.0}',
+        background02: '{Colorprimitives.neutral.50}',
+        background03: '{Colorprimitives.neutral.100}',
+        background04: '{Colorprimitives.neutral.200}',
+        background05: '{Colorprimitives.neutral.300}',
+        background06: '{Colorprimitives.neutral.400}'
+    },
+    bordercolors: {
+        primary: {
+            border100: '{Colorprimitives.primary.500}',
+            border200: '{Colorprimitives.primary.600P}',
+            border300: '{Colorprimitives.primary.700}',
+            border400: '{Colorprimitives.primary.800}',
+            border500: '{Colorprimitives.primary.900}',
+            border600: '{Colorprimitives.primary.950}'
+        },
+        neutral: {
+            border100: '{Colorprimitives.neutral.200}',
+            border200: '{Colorprimitives.neutral.300}',
+            border300: '{Colorprimitives.neutral.400}',
+            border400: '{Colorprimitives.neutral.500}',
+            border500: '{Colorprimitives.neutral.600}',
+            border600: '{Colorprimitives.neutral.700}'
+        }
+    },
+    foregroundcolors: {
+        foreground01: '{Colorprimitives.neutral.950}',
+        foreground02: '{Colorprimitives.neutral.900}',
+        foreground03: '{Colorprimitives.neutral.800}',
+        foreground04: '{Colorprimitives.neutral.700}',
+        foreground05: '{Colorprimitives.neutral.600}',
+        foreground06: '{Colorprimitives.neutral.500}'
+    },
+    semanticcolors: {
+        primary: {
+            semanticPrimary950: '{colors.primary.950}',
+            semanticPrimary900: '{colors.primary.900}',
+            semanticPrimary800: '{colors.primary.800}',
+            semanticPrimary700: '{colors.primary.700}',
+            semanticPrimary600: '{colors.primary.600}',
+            semanticPrimary500: '{colors.primary.500}',
+            semanticPrimary400: '{colors.primary.400}',
+            semanticPrimary300: '{colors.primary.300}',
+            semanticPrimary200: '{colors.primary.200}',
+            semanticPrimary100: '{colors.primary.100}',
+            semanticPrimary50: '{colors.primary.50}'
+        },
+        success: {
+            semanticSuccess950: '{Colorprimitives.green.950}',
+            semanticSuccess900: '{Colorprimitives.green.900}',
+            semanticSuccess800: '{Colorprimitives.green.800}',
+            semanticSuccess700: '{Colorprimitives.green.700}',
+            semanticSuccess600: '{Colorprimitives.green.600}',
+            semanticSuccess500: '{Colorprimitives.green.500}',
+            semanticSuccess400: '{Colorprimitives.green.400}',
+            semanticSuccess300: '{Colorprimitives.green.300}',
+            semanticSuccess200: '{Colorprimitives.green.200}',
+            semanticSuccess100: '{Colorprimitives.green.100}',
+            semanticSuccess50: '{Colorprimitives.green.50}'
+        }
+    }
+};
