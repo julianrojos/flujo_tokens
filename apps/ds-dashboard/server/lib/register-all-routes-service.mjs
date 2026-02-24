47f1542347501e4b79211e2cc612baa66eb171ba
julianrojos <julianrojos@gmail.com>
2026-02-24 22:21:16 +0100

    refactor(ds-dashboard): centralize route dependency wiring

---
+++ apps/ds-dashboard/server/lib/register-all-routes-service.mjs
@@ -0,0 +1,92 @@
+export function buildSharedSystemContextDeps(deps) {
+  return {
+    failJson: deps.failJson,
+    getSystemContext: deps.getSystemContext,
+  };
+}
+
+export function buildAllRouteDeps(deps) {
+  const sharedSystemContextDeps = buildSharedSystemContextDeps(deps);
+
+  return {
+    systemDeps: {
+      buildHealthPayload: deps.buildHealthPayload,
+      failJson: deps.failJson,
+      readJsonBody: deps.readJsonBody,
+      designSystemRepository: deps.designSystemRepository,
+      normalizeSystemId: deps.normalizeSystemId,
+      ensureRelativeDir: deps.ensureRelativeDir,
+      normalizeFigmaApiTokenRef: deps.normalizeFigmaApiTokenRef,
+      normalizeCollectionList: deps.normalizeCollectionList,
+      summarizeDesignSystemsConfig: deps.summarizeDesignSystemsConfig,
+      resolveSafeSystemPathsForDeletion: deps.resolveSafeSystemPathsForDeletion,
+      repoRoot: deps.repoRoot,
+      fsSync: deps.fsSync,
+    },
+    operationsDeps: {
+      failJson: deps.failJson,
+      toFiniteTimestamp: deps.toFiniteTimestamp,
+      OPS_HISTORY_MAX_LIMIT: deps.OPS_HISTORY_MAX_LIMIT,
+      OPS_HISTORY_DEFAULT_LIMIT: deps.OPS_HISTORY_DEFAULT_LIMIT,
+      OPS_REGRESSION_MAX_LIMIT: deps.OPS_REGRESSION_MAX_LIMIT,
+      OPS_REGRESSION_DEFAULT_LIMIT: deps.OPS_REGRESSION_DEFAULT_LIMIT,
+      OPS_REGRESSION_DEFAULT_MIN_SAMPLES: deps.OPS_REGRESSION_DEFAULT_MIN_SAMPLES,
+      designSystemRepository: deps.designSystemRepository,
+      readOperationHistory: deps.readOperationHistory,
+      buildOperationRegressionsReport: deps.buildOperationRegressionsReport,
+      createApiRequestId: deps.createApiRequestId,
+      readJsonBody: deps.readJsonBody,
+      normalizeSystemId: deps.normalizeSystemId,
+      findOperationEventById: deps.findOperationEventById,
+      enqueueReplayJobFromOperation: deps.enqueueReplayJobFromOperation,
+      queueJobAcceptedPayload: deps.queueJobAcceptedPayload,
+    },
+    registryDeps: sharedSystemContextDeps,
+    tokenGraphDeps: sharedSystemContextDeps,
+    healthDeps: sharedSystemContextDeps,
+    analysisDeps: sharedSystemContextDeps,
+    componentSpecDeps: {
+      ...sharedSystemContextDeps,
+      isDevRuntime: deps.isDevRuntime,
+      readJsonBody: deps.readJsonBody,
+      resolveRepoFilePath: deps.resolveRepoFilePath,
+      sha256Text: deps.sha256Text,
+    },
+    fileDeps: {
+      ...sharedSystemContextDeps,
+      resolveRepoFilePath: deps.resolveRepoFilePath,
+      readTextFileLimited: deps.readTextFileLimited,
+      findLineForQuery: deps.findLineForQuery,
+      buildSnippet: deps.buildSnippet,
+      guessContentType: deps.guessContentType,
+      MAX_FILE_BYTES: deps.MAX_FILE_BYTES,
+    },
+    jobDeps: {
+      failJson: deps.failJson,
+      queueJobs: deps.queueJobs,
+      listQueueJobEvents: deps.listQueueJobEvents,
+      queueJobSnapshot: deps.queueJobSnapshot,
+      isQueueJobFinalStatus: deps.isQueueJobFinalStatus,
+      cancelQueueJob: deps.cancelQueueJob,
+      toQueueTerminalEvent: deps.toQueueTerminalEvent,
+      buildApiErrorPayload: deps.buildApiErrorPayload,
+      MAX_RETAINED_EVENTS: deps.MAX_RETAINED_EVENTS,
+    },
+    commandDeps: {
+      failJson: deps.failJson,
+      createApiRequestId: deps.createApiRequestId,
+      readJsonBody: deps.readJsonBody,
+      getSystemContext: deps.getSystemContext,
+      queueJobAcceptedPayload: deps.queueJobAcceptedPayload,
+      enqueueQueueJob: deps.enqueueQueueJob,
+      sha256Text: deps.sha256Text,
+      runQueuedSpawnCommand: deps.runQueuedSpawnCommand,
+      queueNpmScript: deps.queueNpmScript,
+      enqueueRefreshNamingDebtJob: deps.enqueueRefreshNamingDebtJob,
+      queueNodeJsonCommand: deps.queueNodeJsonCommand,
+      toBooleanString: deps.toBooleanString,
+      toNumberString: deps.toNumberString,
+      validateGitRef: deps.validateGitRef,
+    },
+  };
+}
--- apps/ds-dashboard/server/routes/register-all-routes.mjs
+++ apps/ds-dashboard/server/routes/register-all-routes.mjs
@@ -8,108 +8,19 @@ import { registerComponentSpecRoutes } from "./component-spec-routes.mjs";
 import { registerFileRoutes } from "./file-routes.mjs";
 import { registerJobRoutes } from "./job-routes.mjs";
 import { registerCommandRoutes } from "./command-routes.mjs";
+import { buildAllRouteDeps } from "../lib/register-all-routes-service.mjs";
 
 export function registerAllRoutes(app, deps) {
-  registerSystemRoutes(app, {
-    buildHealthPayload: deps.buildHealthPayload,
-    failJson: deps.failJson,
-    readJsonBody: deps.readJsonBody,
-    designSystemRepository: deps.designSystemRepository,
-    normalizeSystemId: deps.normalizeSystemId,
-    ensureRelativeDir: deps.ensureRelativeDir,
-    normalizeFigmaApiTokenRef: deps.normalizeFigmaApiTokenRef,
-    normalizeCollectionList: deps.normalizeCollectionList,
-    summarizeDesignSystemsConfig: deps.summarizeDesignSystemsConfig,
-    resolveSafeSystemPathsForDeletion: deps.resolveSafeSystemPathsForDeletion,
-    repoRoot: deps.repoRoot,
-    fsSync: deps.fsSync,
-  });
+  const routeDeps = buildAllRouteDeps(deps);
 
-  registerOperationsRoutes(app, {
-    failJson: deps.failJson,
-    toFiniteTimestamp: deps.toFiniteTimestamp,
-    OPS_HISTORY_MAX_LIMIT: deps.OPS_HISTORY_MAX_LIMIT,
-    OPS_HISTORY_DEFAULT_LIMIT: deps.OPS_HISTORY_DEFAULT_LIMIT,
-    OPS_REGRESSION_MAX_LIMIT: deps.OPS_REGRESSION_MAX_LIMIT,
-    OPS_REGRESSION_DEFAULT_LIMIT: deps.OPS_REGRESSION_DEFAULT_LIMIT,
-    OPS_REGRESSION_DEFAULT_MIN_SAMPLES: deps.OPS_REGRESSION_DEFAULT_MIN_SAMPLES,
-    designSystemRepository: deps.designSystemRepository,
-    readOperationHistory: deps.readOperationHistory,
-    buildOperationRegressionsReport: deps.buildOperationRegressionsReport,
-    createApiRequestId: deps.createApiRequestId,
-    readJsonBody: deps.readJsonBody,
-    normalizeSystemId: deps.normalizeSystemId,
-    findOperationEventById: deps.findOperationEventById,
-    enqueueReplayJobFromOperation: deps.enqueueReplayJobFromOperation,
-    queueJobAcceptedPayload: deps.queueJobAcceptedPayload,
-  });
-
-  registerRegistryRoutes(app, {
-    failJson: deps.failJson,
-    getSystemContext: deps.getSystemContext,
-  });
-
-  registerTokenGraphRoutes(app, {
-    failJson: deps.failJson,
-    getSystemContext: deps.getSystemContext,
-  });
-
-  registerHealthRoutes(app, {
-    failJson: deps.failJson,
-    getSystemContext: deps.getSystemContext,
-  });
-
-  registerAnalysisRoutes(app, {
-    failJson: deps.failJson,
-    getSystemContext: deps.getSystemContext,
-  });
-
-  registerComponentSpecRoutes(app, {
-    failJson: deps.failJson,
-    getSystemContext: deps.getSystemContext,
-    isDevRuntime: deps.isDevRuntime,
-    readJsonBody: deps.readJsonBody,
-    resolveRepoFilePath: deps.resolveRepoFilePath,
-    sha256Text: deps.sha256Text,
-  });
-
-  registerFileRoutes(app, {
-    failJson: deps.failJson,
-    getSystemContext: deps.getSystemContext,
-    resolveRepoFilePath: deps.resolveRepoFilePath,
-    readTextFileLimited: deps.readTextFileLimited,
-    findLineForQuery: deps.findLineForQuery,
-    buildSnippet: deps.buildSnippet,
-    guessContentType: deps.guessContentType,
-    MAX_FILE_BYTES: deps.MAX_FILE_BYTES,
-  });
-
-  registerJobRoutes(app, {
-    failJson: deps.failJson,
-    queueJobs: deps.queueJobs,
-    listQueueJobEvents: deps.listQueueJobEvents,
-    queueJobSnapshot: deps.queueJobSnapshot,
-    isQueueJobFinalStatus: deps.isQueueJobFinalStatus,
-    cancelQueueJob: deps.cancelQueueJob,
-    toQueueTerminalEvent: deps.toQueueTerminalEvent,
-    buildApiErrorPayload: deps.buildApiErrorPayload,
-    MAX_RETAINED_EVENTS: deps.MAX_RETAINED_EVENTS,
-  });
-
-  registerCommandRoutes(app, {
-    failJson: deps.failJson,
-    createApiRequestId: deps.createApiRequestId,
-    readJsonBody: deps.readJsonBody,
-    getSystemContext: deps.getSystemContext,
-    queueJobAcceptedPayload: deps.queueJobAcceptedPayload,
-    enqueueQueueJob: deps.enqueueQueueJob,
-    sha256Text: deps.sha256Text,
-    runQueuedSpawnCommand: deps.runQueuedSpawnCommand,
-    queueNpmScript: deps.queueNpmScript,
-    enqueueRefreshNamingDebtJob: deps.enqueueRefreshNamingDebtJob,
-    queueNodeJsonCommand: deps.queueNodeJsonCommand,
-    toBooleanString: deps.toBooleanString,
-    toNumberString: deps.toNumberString,
-    validateGitRef: deps.validateGitRef,
-  });
+  registerSystemRoutes(app, routeDeps.systemDeps);
+  registerOperationsRoutes(app, routeDeps.operationsDeps);
+  registerRegistryRoutes(app, routeDeps.registryDeps);
+  registerTokenGraphRoutes(app, routeDeps.tokenGraphDeps);
+  registerHealthRoutes(app, routeDeps.healthDeps);
+  registerAnalysisRoutes(app, routeDeps.analysisDeps);
+  registerComponentSpecRoutes(app, routeDeps.componentSpecDeps);
+  registerFileRoutes(app, routeDeps.fileDeps);
+  registerJobRoutes(app, routeDeps.jobDeps);
+  registerCommandRoutes(app, routeDeps.commandDeps);
 }
